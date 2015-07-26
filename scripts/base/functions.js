5720011d70a22342130dfb4a70f328224aa78e9e
powleeno <jp.brites@gmail.com>
2015-07-26 09:09:47 -0400

    added stretch object funcionality

--- scripts/base/functions.js
+++ scripts/base/functions.js
@@ -1,81 +1,56 @@
-function stretch_image(element) {
-    $(element).each(function(){
+function stretch_object() {
 
-        var main_column = $(this).find('.main');
-	    var secondary_column = $(this).find('.secondary');
-        var image = secondary_column.find('.image');
+    $('.str-obj').each(function () {
 
-        // Size secondary column height ------------------------------------------
-        var secondary_column_height;
-        if ( $(window).width() > window_small ) { // large and medium screen size
-            if( main_column.outerHeight() > secondary_column.innerWidth() ) {
-                secondary_column_height = main_column.outerHeight();
+        var str_obj = $(this),
+            str_con = str_obj.parent('.str-con'),
+            str_ref = str_con.parent().find('.str-ref');
+
+        // Set fundamental str_obj styles -----------------------------------------
+        str_obj.css({
+            'position': 'absolute',
+            'background-size': 'cover',
+            'background-position': '50%',
+            'height': '100%'
+        });
+
+        // Set str_con height -----------------------------------------------------
+        var str_con_height;
+
+        if (Foundation.utils.is_medium_up()) { // large and medium screen size
+            if (str_ref.innerHeight() > str_con.innerWidth()) {
+                str_con_height = str_ref.innerHeight();
             } else {
-                secondary_column_height = secondary_column.innerWidth();
+                str_con_height = str_con.innerWidth();
             }
         } else { // small screen sizes
-            secondary_column_height = secondary_column.innerWidth();
+            str_con_height = str_con.innerWidth();
         }
-        secondary_column.outerHeight( secondary_column_height );
+        str_con.outerHeight(str_con_height);
 
-        // Size image width and set margin ---------------------------------------
-        var image_width, margin_left;
-        if( image.hasClass("ratio-13") || image.hasClass("ratio-22") ) {
-            image_width = secondary_column.offset().left + secondary_column.innerWidth();
-            margin_left = 0 - secondary_column.offset().left - image.position().left;
-        } else if( image.hasClass("ratio-31") ) {
-            image_width = $(window).width() - secondary_column.offset().left;
-            margin_left = 0 - image.position().left;
+
+        var str_obj_width, str_obj_margin_left;
+        if (str_obj.hasClass('top')) {
+
+            // Set str_obj width and margin -------------------------------------------
+
+            if (str_obj.hasClass('left')) {
+                str_obj_width = str_con.offset().left + str_con.innerWidth();
+                str_obj_margin_left = 0 - str_con.offset().left - str_obj.position().left;
+            } else if (str_obj.hasClass('right')) {
+                str_obj_width = $(window).width() - str_con.offset().left;
+                str_obj_margin_left = 0 - str_obj.position().left;
+            }
+            str_obj.outerWidth(str_obj_width);
+            str_obj.css('margin-left', str_obj_margin_left);
+
+        } else if (str_obj.hasClass('box')) {
+
+            str_obj_width = str_con.innerWidth();
+            str_obj.outerWidth(str_obj_width);
+
         }
-        image.outerWidth(image_width);
-        image.css("margin-left", margin_left);
 
     });
-}
 
-
-
-function stretch_box(element) {
-    $(element).each(function(){
-
-        var main_column = $(this).find('.main');
-        var secondary_column = $(this).find('.secondary');
-        var main_column_text = main_column.find('.text-wrapper');
-        var secondary_column_text = secondary_column.find('.text-wrapper');
-        var back = secondary_column.find('.back');
-
-        // Size secondary column height ------------------------------------------
-        var main_column_height, secondary_column_height;
-        if ( $(window).width() > window_small ) { // large and medium screen size
-            if ( main_column_text.height() > secondary_column_text.height() ) {
-                main_column_height = secondary_column_height = main_column_text.height() + main_column_text.position().top * 2;
-            } else if ( main_column_text.height() < secondary_column_text.height() ) {
-                main_column_height = secondary_column_height = secondary_column_text.height() + secondary_column_text.position().top * 2;
-            }
-        } else { // small screen sizes
-            main_column_height = main_column_text.height() + main_column_text.position().top * 2;
-            secondary_column_height = secondary_column_text.height() + secondary_column_text.position().top * 2;
-        }
-        main_column.outerHeight( main_column_height );
-        secondary_column.outerHeight( secondary_column_height );
-
-        // Size back width and height and set margin -----------------------------
-        var back_width, margin_left;
-        if ( $(window).width() > window_medium ) { // large screen size
-            if( back.hasClass("ratio-13") || back.hasClass("ratio-22") ) {
-                back_width = secondary_column.offset().left + secondary_column.innerWidth() + box_back_padding;
-                margin_left = 0 - secondary_column.offset().left - back.position().left;
-            } else if( back.hasClass("ratio-31") ) {
-                back_width = $(window).width() - secondary_column.offset().left + box_back_padding;
-                margin_left = 0 - back.position().left - box_back_padding;
-            }
-        } else { // medium and small screen sizes
-            back_width = secondary_column.innerWidth();
-            margin_left = 0 - back.position().left;
-        }
-        back.outerWidth( back_width );
-        back.css( "margin-left", margin_left );
-        back.css( "margin-top", 0 - secondary_column_text.position().top );
-
-    });
 }
