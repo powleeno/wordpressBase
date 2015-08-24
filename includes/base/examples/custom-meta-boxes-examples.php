ed370fe61d4d12f037df97b69bf72e1a03377a5e
powleeno <jp.brites@gmail.com>
2015-08-24 18:32:59 -0400

    minor edits

--- includes/base/examples/custom-meta-boxes-examples.php
+++ includes/base/examples/custom-meta-boxes-examples.php
@@ -6,11 +6,11 @@
 
 // Example #1 -----------------------------------
 
-$meta_boxes['leftColumn'] = array(
-	'id' => 'leftColumn',
+$meta_boxes['left_column'] = array(
+	'id' => 'left_column',
 	'title' => __('Left Column', 'custom metaboxes'),
-	'object_types' => array('subjects',),
-	// 'show_on'       => array( 'key' => 'page-template', 'value' => $pagesTwoColumns ),
+	// 'object_types' => array('page'),
+	// 'priority' => 'high',
 	'context' => 'normal',
 	'priority' => 'high',
 	'show_names' => false,
@@ -28,11 +28,11 @@ $meta_boxes['leftColumn'] = array(
 
 // Example #2 -----------------------------------
 
-$meta_boxes['rightColumn'] = array(
-	'id' => 'rightColumn',
+$meta_boxes['right_column'] = array(
+	'id' => 'right_column',
 	'title' => __('Right Column', 'custom metaboxes'),
-	'object_types' => array('subjects',),
-	// 'show_on'       => array( 'key' => 'page-template', 'value' => $pagesTwoColumns ),
+	// 'object_types' => array('page'),
+	// 'priority' => 'high',
 	'context' => 'normal',
 	'priority' => 'high',
 	'show_names' => false,
@@ -40,6 +40,7 @@ $meta_boxes['rightColumn'] = array(
 		array(
 			'name' => __('Right Column', 'custom metaboxes'),
 			'id' => $base_fields_prefix . 'right_column',
+			'desc' => __('', 'custom metaboxes'),
 			'sanitization_cb' => false,
 			'type' => 'wysiwyg',
 			'options' => $wysiwygFull
@@ -53,16 +54,16 @@ $meta_boxes['rightColumn'] = array(
 $meta_boxes['blocks_section'] = array(
 	'id' => $base_fields_prefix . 'blocks_section',
 	'title' => __('Blocks Section', 'custom metaboxes'),
-	'object_types' => array('page'),
+	// 'object_types' => array('page'),
+	// 'priority' => 'high',
 	'context' => 'normal',
-	'priority' => 'high',
 	'show_on' => array('key' => 'page-template', 'value' => $blocks_pages),
 	'fields' => array(
 		array(
 			'name' => __('Activation', 'custom metaboxes'),
 			'id' => $base_fields_prefix . 'blocks_section_activation',
 			'type' => 'radio_inline',
-			'desc' => __('Select whether this section is to be displayed in the front-end.', 'custom metaboxes'),
+			'desc' => __('', 'custom metaboxes'),
 			'default' => 'active',
 			'options' => array(
 				'active' => __('Active Section', 'custom metaboxes'),
@@ -72,7 +73,7 @@ $meta_boxes['blocks_section'] = array(
 		array(
 			'id' => $base_fields_prefix . 'blocks',
 			'type' => 'group',
-			// 'desc' => __( 'Insert Page Blocks', 'custom metaboxes' ),
+			'desc' => __( '', 'custom metaboxes' ),
 			'options' => array(
 				'group_title' => __('Block #{#}', 'custom metaboxes'), // {#} gets replaced by row number
 				'add_button' => __('Add Another Block', 'custom metaboxes'),
@@ -85,7 +86,7 @@ $meta_boxes['blocks_section'] = array(
 					'name' => __('Layout', 'custom metaboxes'),
 					'id' => $base_fields_prefix . 'block_layout',
 					'type' => 'radio_inline',
-					'desc' => __('Select block layout.', 'custom metaboxes'),
+					'desc' => __('', 'custom metaboxes'),
 					'default' => 'full',
 					'options' => array(
 						'full' => __('Full Width', 'custom metaboxes'),
@@ -97,14 +98,14 @@ $meta_boxes['blocks_section'] = array(
 					'name' => __('Image', 'custom metaboxes'),
 					'id' => $base_fields_prefix . 'block_image',
 					'type' => 'file',
-					'desc' => __('Only available with "Boxed" layout.', 'custom metaboxes'),
+					'desc' => __('', 'custom metaboxes'),
 					'allow' => array('url', 'attachment') // limit to just attachments with array( 'attachment' )
 				),
 				array(
 					'name' => __('Float', 'custom metaboxes'),
 					'id' => $base_fields_prefix . 'block_float',
 					'type' => 'radio_inline',
-					'desc' => __('Select image float. Only available with "Boxed" layout.', 'custom metaboxes'),
+					'desc' => __('', 'custom metaboxes'),
 					'default' => 'left',
 					'options' => array(
 						'left' => __('Image to the left', 'custom metaboxes'),
@@ -114,6 +115,7 @@ $meta_boxes['blocks_section'] = array(
 				array(
 					'name' => __('Main Column', 'custom metaboxes'),
 					'id' => $base_fields_prefix . 'block_main_column',
+					'desc' => __('', 'custom metaboxes'),
 					'sanitization_cb' => false,
 					'type' => 'wysiwyg',
 					'options' => $wysiwygFull
@@ -121,7 +123,7 @@ $meta_boxes['blocks_section'] = array(
 				array(
 					'name' => __('Secondary Column', 'custom metaboxes'),
 					'id' => $base_fields_prefix . 'block_secondary_column',
-					'desc' => __('Only available with "Split Width" layout.', 'custom metaboxes'),
+					'desc' => __('', 'custom metaboxes'),
 					'sanitization_cb' => false,
 					'type' => 'wysiwyg',
 					'options' => $wysiwygFull
@@ -134,25 +136,25 @@ $meta_boxes['blocks_section'] = array(
 
 // Example #4 -----------------------------------
 
-$meta_boxes['locations'] = array(
-	'id' => 'locations',
-	'title' => __('Location', 'custom metaboxes'),
-	'object_types' => array('locations',),
-	// 'show_on'       => array( 'key' => 'post', 'value' => 'locations' ),
+$meta_boxes['map_locations'] = array(
+	'id' => 'map_locations',
+	'title' => __('Map Location', 'custom metaboxes'),
+	// 'object_types' => array('page'),
+	// 'priority' => 'high',
 	'context' => 'normal',
 	'priority' => 'high',
 	'show_names' => true,
 	'fields' => array(
 		array(
 			'name' => __('Address', 'custom metaboxes'),
-			'id' => $base_fields_prefix . 'address',
+			'id' => $base_fields_prefix . 'map_location_address',
 			'desc' => __('', 'custom metaboxes'),
 			'sanitization_cb' => false,
 			'type' => 'text',
 		),
 		array(
 			'name' => __('Information', 'custom metaboxes'),
-			'id' => $base_fields_prefix . 'information',
+			'id' => $base_fields_prefix . 'map_location_information',
 			'desc' => __('', 'custom metaboxes'),
 			'sanitization_cb' => false,
 			'type' => 'wysiwyg',
